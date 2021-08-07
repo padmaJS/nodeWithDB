d6996200482af66ba339a44660875e9c062ad947
padmaJS <64828947+padmaJS@users.noreply.github.com>
2021-08-07 19:30:34 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql')
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('./public'))
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: ''
+})
+
+app.get('/', (req,res) => {
+  let sql = 'SELECT * FROM movielist'
+  let query = db.query(sql,(err,result) => {
+    if (err) throw err
+    res.json({genre: result})
+  })
+  res.json({id:1, title: 'Slice Of Life'})
+})
+
+/*db.connect((err) => {
+  if(err) throw err;
+  console.log('Database connected');
+})*/
+
+app.listen(3000,() => {
+  console.log('listening on port 3000...');
+})
